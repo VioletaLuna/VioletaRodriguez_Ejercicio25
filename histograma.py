47ae4ec17f96037a4b5c186e09ebe0d0ca0db2e9
Violeta Rodríguez <violetar48@gmail.com>
2018-11-06 23:00:12 -0500

    Funciona todo perfecto, pero no estoy segura si es la forma de pedir los datos: se pide uno por uno

--- histograma.py
+++ histograma.py
@@ -4,10 +4,8 @@ import matplotlib.pyplot as plt
 
 data = np.genfromtxt("sample.dat", delimiter = " ", skip_header = 0)
 muestra = data
-print(muestra.size)
 
 
 plt.hist(muestra )
 
-plt.show()
-#plt.savefig("grafica.png")
+plt.savefig("sample.pdf")
